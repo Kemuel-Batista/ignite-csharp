31ebcf969457df8141e458e5b1f61f9af7ddb8bd
Kemuel Batista <kemuellima20@gmail.com>
2024-03-21 14:37:43 -0300

    csharp-hello-world

---
+++ csharp-01-hello-world/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_01_hello_world;
+class Carro
+{
+    void Ligar()
+    {
+        Console.WriteLine("Carro ligado!");
+    }
+
+    void Desligar()
+    {
+        Console.WriteLine("Carro desligado!");
+    }
+}
---
+++ csharp-01-hello-world/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace csharp_01_hello_world;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
